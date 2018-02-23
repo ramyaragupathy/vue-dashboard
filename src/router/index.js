47bdaaa91d0cb1b8d76e2ca85ee5c0fd653156dd
rammmukul <rammmukul@outlook.com>
2018-02-24 02:29:09 +0530

    Refactor

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Profile from '@/components/Profile'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/profile',
       name: 'profile',
