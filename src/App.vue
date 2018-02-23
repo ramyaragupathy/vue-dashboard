4b289ecce9db1e8ab8e1cd9aa8ca8b2905a9de0c
Donna Ann Issac <donna.ann1209@gmail.com>
2018-02-24 02:09:36 +0530

    Add child components to parent container

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <left-nav></left-nav>
+    <content-div></content-div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import leftNav from './components/leftNav.vue'
+import contentDiv from './components/contentDiv.vue'
+Vue.component('left-nav', leftNav)
+Vue.component('content-div', contentDiv)
+
 export default {
   name: 'App'
 }
