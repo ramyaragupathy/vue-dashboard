955056689a46ef1b1bb67ddba6f5d0786bafd0af
Donna Ann Issac <donna.ann1209@gmail.com>
2018-02-24 03:08:15 +0530

    Correct filename error

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Profile from '@/components/Profile'
-import Report from '@/components/Report'
+import Report from '@/components/report'
 
 Vue.use(Router)
 
