e89f9c2aa4c9cad1fd916ca475702037bb9b4425
Donna Ann Issac <donna.ann1209@gmail.com>
2018-02-24 06:05:20 +0530

    Save appState

---
+++ src/appData.js
@@ -0,0 +1,25 @@
+export default {
+    current: {},
+    dashboard: {
+      stats: {
+        projects: 10,
+        leaves: 4,
+        since: 40
+      },
+      activity: [],
+      dailyreport: "nothing to show",
+      upcoming:"Launch nuclear missles"
+    },
+    dreport: {
+      reports: [
+        {date: "23/2/18", time : "8:59 AM", content:"- worked on code editor for the app - fixed some css for code mirror - progress bar for challenge increases when solved any problem - app flow now works properly - evaluating code type still remains https://github.com/sumitkumar15/kyurious "},
+        {date: "24/2/18", time : "8:51 AM", content:"- worked on code editor for the app - fi when solved any problem - app flow now works properly - evaluating code type still remains https://github.com/sumitkumar15/kyurious "},
+        {date: "25/2/18", time : "8:59 AM", content:"- worked on code editor for the app - fily - evaluating code type still remains https://github.com/sumitkumar15/kyurious "},
+        {date: "26/2/18", time : "8:56 AM", content:"- worked on code editor for the app - fixed some css for code mirror - progress bar for challenge increases when solved any problem - app flow now works properly - evaluating code type still  "},
+        {date: "27/2/18", time : "8:00 AM", content:"- worked on code editor for the app - fixed some css for code mirror - progress bar for cremains https://github.com/sumitkumar15/kyurious "}
+      ]
+    },
+    demo: {},
+    info: {},
+    project: {}
+}
