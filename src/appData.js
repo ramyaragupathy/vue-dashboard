93dcf0e8a788cc02f637781f3fb18eb66c15f11f
Ramya <ramya@mapbox.com>
2018-02-24 08:44:30 +0530

    support data for md conversion

--- src/appData.js
+++ src/appData.js
@@ -1,25 +1,25 @@
 export default {
-    current: {},
-    dashboard: {
-      stats: {
-        projects: 10,
-        leaves: 4,
-        since: 40
-      },
-      activity: [],
-      dailyreport: "nothing to show",
-      upcoming:"Launch nuclear missles"
+  current: {},
+  dashboard: {
+    stats: {
+      projects: 10,
+      leaves: 4,
+      since: 40
     },
-    dreport: {
-      reports: [
-        {date: "23/2/18", time : "8:59 AM", content:"- worked on code editor for the app - fixed some css for code mirror - progress bar for challenge increases when solved any problem - app flow now works properly - evaluating code type still remains https://github.com/sumitkumar15/kyurious "},
-        {date: "24/2/18", time : "8:51 AM", content:"- worked on code editor for the app - fi when solved any problem - app flow now works properly - evaluating code type still remains https://github.com/sumitkumar15/kyurious "},
-        {date: "25/2/18", time : "8:59 AM", content:"- worked on code editor for the app - fily - evaluating code type still remains https://github.com/sumitkumar15/kyurious "},
-        {date: "26/2/18", time : "8:56 AM", content:"- worked on code editor for the app - fixed some css for code mirror - progress bar for challenge increases when solved any problem - app flow now works properly - evaluating code type still  "},
-        {date: "27/2/18", time : "8:00 AM", content:"- worked on code editor for the app - fixed some css for code mirror - progress bar for cremains https://github.com/sumitkumar15/kyurious "}
-      ]
-    },
-    demo: {},
-    info: {},
-    project: {}
+    activity: [],
+    dailyreport: "nothing to show",
+    upcoming:"Launch nuclear missles"
+  },
+  dreport: {
+    reports: [
+      {date: "23/2/18", time : "8:59 AM", content:`[test](https://github.com/sumitkumar15/kyurious)`},
+      {date: "24/2/18", time : "8:51 AM", content:"- worked on code editor for the app\n- fi when solved any problem \n- app flow now works properly \n-  evaluating code type still remains https://github.com/sumitkumar15/kyurious "},
+      {date: "25/2/18", time : "8:59 AM", content:"- worked on code editor for the app\n- fi when solved any problem \n- app flow now works properly \n-  evaluating code type still remains https://github.com/sumitkumar15/kyurious "},
+      {date: "26/2/18", time : "8:56 AM", content:"- worked on code editor for the app\n - fixed some css for code mirror\n - progress bar for challenge increases when solved any problem\n - app flow now works properly\n- evaluating code type still  "},
+      {date: "27/2/18", time : "8:00 AM", content:"- worked on code editor for the app\n - fixed some css for code mirror\n - progress bar for cremains https://github.com/sumitkumar15/kyurious "}
+    ]
+  },
+  demo: {},
+  info: {},
+  project: {}
 }
