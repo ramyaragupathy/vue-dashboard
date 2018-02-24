521836f4733b37c452d4eda438e7a34bf9e61887
Donna Ann Issac <donna.ann1209@gmail.com>
2018-02-24 06:04:05 +0530

    Child component to display each report

---
+++ src/components/reportCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    <div class='card'>
+        <header class='card-header'>
+            <p class='card-header-title'>
+                {{reportData.date }} {{reportData.time}}
+            </p>
+
+        </header>
+    </div class='card-content'>
+    <div class='content'>
+        <div class='control has-icons-left has-icons-right'>
+            {{reportData.content}}
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['reportData']
+    }
+</script>
